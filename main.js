3218b8ee698c4b1625e73cd4f4093f75568fd4bf
Evangelos Roudosis <roudosisevang@gmail.com>
2025-10-20 17:15:27 +0300

    Content update with css

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function  menuToggle() {
+    document.getElementById("sideNavBar").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("closeBtn").style.width= "0";
+}
